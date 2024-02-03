1a5de8601d57bffea3ca564c0f161ef5fc7a5834
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2024-02-03 09:16:15 -0300

    Add myoro_bet_tracker/deployment_scripts/mac.sh

---
+++ myoro_bet_tracker/deployment_scripts/mac.sh
@@ -0,0 +1,15 @@
+#!/bash/bash
+# Run this within Git Bash
+
+clear
+
+cd "$(dirname "$0")/.."
+
+if [ -d "build" ]; then
+  rm -rf build
+fi
+
+flutter pub cache clean -f
+flutter clean
+flutter pub get
+flutter build macos --release
